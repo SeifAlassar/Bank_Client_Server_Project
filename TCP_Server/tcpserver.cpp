b56104f30a497bd901cb7c6fe2e4cca3a0a74985
SeifAlassar <132224679+SeifAlassar@users.noreply.github.com>
2024-05-19 00:03:05 +0300

    Update tcpserver.cpp

--- TCP_Server/tcpserver.cpp
+++ TCP_Server/tcpserver.cpp
@@ -1,4 +1,4 @@
-#include "TcpServer.h"
+#include "tcpserver.h"
 #include "JsonDatabase.h"
 #include <QDebug>
 #include <QTcpSocket>
@@ -8,7 +8,7 @@
 TcpServer::TcpServer(QObject *parent)
     : QTcpServer(parent) {}
 
-void TcpServer::startServer(int port) {
+void TcpServer::startServer(int port) {             //takes port number and print in server terminal if server is started or not and the port number
     if (!this->listen(QHostAddress::Any, port)) {
         qDebug() << "Could not start server on port" << port;
     } else {
@@ -17,27 +17,43 @@ void TcpServer::startServer(int port) {
 }
 
 void TcpServer::incomingConnection(qintptr socketDescriptor) {
-    QTcpSocket *socket = new QTcpSocket(this);
-    if (socket->setSocketDescriptor(socketDescriptor)) {
-        connect(socket, &QTcpSocket::readyRead, this, [this, socket]() { this->handleClient(socket); });
+    QTcpSocket *socket = new QTcpSocket(this);                                  //create new Qtcp object
+    if (socket->setSocketDescriptor(socketDescriptor)) {                         //bind the tcpsocket to the socketdes connection
+        connect(socket, &QTcpSocket::readyRead, this, &TcpServer::readyRead);       //if binding is successful connect the signals
+        connect(socket, &QTcpSocket::disconnected, this, &TcpServer::disconnected);
         qDebug() << "New connection from" << socket->peerAddress().toString();
     } else {
-        delete socket;
+        delete socket;                                                              //delete socket if fails
     }
 }
 
-void TcpServer::handleClient(QTcpSocket* socket) {
-    QJsonObject request = readJsonFromSocket(socket);
-    QString type = request["type"].toString();
-    QJsonObject response;
-    qDebug() << "Request type:" << type;
+void TcpServer::readyRead()
+{
+    QTcpSocket* socket = qobject_cast<QTcpSocket*>(sender());
+    if (!socket) {
+        qDebug() << "Invalid socket in readyRead";
+        return;
+    }
 
+    QByteArray jsonData = socket->readAll();
+    QJsonDocument document = QJsonDocument::fromJson(jsonData);   //parsing the json data
+    QJsonObject request = document.object();                     //extracting the json object from json document
+
+    QJsonObject response;
+    QString type = request["type"].toString();
+
+    handleClient(socket, request, response);
+}
+
+void TcpServer::handleClient(QTcpSocket* socket, const QJsonObject& request, QJsonObject& response)
+{
+    QString type = request["type"].toString();
     if (type == "login") {
         QString username = request["username"].toString();
         QString password = request["password"].toString();
         QString role;
 
-        bool authenticated = JsonDatabase::authenticateUser(username, password , role);
+        bool authenticated = JsonDatabase::authenticateUser(username, password, role);
         qDebug() << "Username:" << username << "Password:" << password;
         if (authenticated) {
             response["status"] = "success";
@@ -46,42 +62,132 @@ void TcpServer::handleClient(QTcpSocket* socket) {
             response["account_number"] = JsonDatabase::getAccountNumber(username);
             response["balance"] = JsonDatabase::getAccountBalance(username);
 
-            if(role=="admin")
-            {
-            QJsonObject Database = JsonDatabase::loadDatabase();
-            QJsonArray accounts = Database["accounts"].toArray();
-            response["accounts"]=QJsonValue (accounts);
+            if (role == "admin") {
+                QJsonObject Database = JsonDatabase::loadDatabase();
+                QJsonArray accounts = Database["accounts"].toArray();
+                response["accounts"] = QJsonValue(accounts);
             }
-
-            // QString username = request["username"].toString();
-            double amount = request["amount"].toDouble();
-            bool result = JsonDatabase::makeTransaction(username, amount);
-            if (result) {
-                response["status"] = "success";
-                response["message"] = "Transaction successful";
-                response["balance"] = JsonDatabase::getAccountBalance(username);
-            }
-
-            QJsonArray transactions = JsonDatabase::getTransactionHistory(username);
-            if (!transactions.isEmpty()) {
-                response["status"] = "success";
-                response["transactions"] = transactions;
-            }
-
-
-
-        }
-
-        else {
+        } else {
             response["status"] = "failure";
             response["message"] = "Authentication failed";
         }
 
-        qDebug() << "Response:" << response;
         sendJsonToSocket(socket, response);
 
-        //socket->disconnectFromHost();
+    } else if (type == "create_user") {
+        QString username = request["username"].toString();
+        QString accountNumber = request["account_number"].toString();
+        QString password = request["password"].toString();
+        QString role = request["role"].toString();
+        QString fullName = request["full_name"].toString();
+        int age = request["age"].toInt();
+
+        bool created = JsonDatabase::createUser(username, accountNumber, password, role, fullName, age);
+        if (created) {
+            response["status"] = "success";
+            response["message"] = "User created successfully";
+        } else {
+            response["status"] = "failure";
+            response["message"] = "User already exists";
+        }
+
+        sendJsonToSocket(socket, response);
+
     }
+    else if (type == "delete_user") {
+        QString accountNumber = request["account_number"].toString();
+
+        bool deleted = JsonDatabase::deleteUser(accountNumber);
+        if (deleted) {
+            response["status"] = "success";
+            response["message"] = "User deleted successfully";
+        } else {
+            response["status"] = "failure";
+            response["message"] = "User not found";
+        }
+        sendJsonToSocket(socket, response);
+    }
+    else if (type == "update_user") {
+        QString accountNumber = request["account_number"].toString();
+        QString password = request["password"].toString();
+        QString fullName = request["full_name"].toString();
+        int age = request["age"].toInt();
+
+        bool updated = JsonDatabase::updateUser(accountNumber, password, fullName, age);
+        if (updated) {
+            response["status"] = "success";
+            response["message"] = "User updated successfully";
+        } else {
+            response["status"] = "failure";
+            response["message"] = "User not found";
+        }
+        sendJsonToSocket(socket, response);
+    }
+
+    else if (type == "get_balance") {
+        QString username = request["username"].toString();
+        double balance = JsonDatabase::getAccountBalance(username);
+        response["status"] = "success";
+        response["balance"] = balance;
+        sendJsonToSocket(socket, response);
+    } else if (type == "get_account_number") {
+        QString username = request["username"].toString();
+        QString accountNumber = JsonDatabase::getAccountNumber(username);
+        response["status"] = "success";
+        response["account_number"] = accountNumber;
+        sendJsonToSocket(socket, response);
+    }  else if (type == "get_transaction_history") {
+        QString username = request["username"].toString();
+        QJsonArray transactions = JsonDatabase::getTransactionHistory(username);
+        response["status"] = "success";
+        response["transactions"] = transactions;
+        sendJsonToSocket(socket, response);
+
+    }else if (type == "get_database") {
+        QJsonObject Database = JsonDatabase::loadDatabase();
+        QJsonArray accounts = Database["accounts"].toArray();
+        response["status"] = "success";
+        response["accounts"] = accounts;
+        sendJsonToSocket(socket, response);
+
+    } else if (type == "make_transaction") {
+        QString username = request["username"].toString();
+        double amount = request["amount"].toDouble();
+        double newBalance = JsonDatabase::makeTransaction(username, amount);
+        response["status"] = "success";
+        response["balance"] = newBalance;
+        sendJsonToSocket(socket, response);
+    }
+    else if (type == "transfer_amount") {
+        QString fromAccountNumber = request["from_account_number"].toString();
+        QString toAccountNumber = request["to_account_number"].toString();
+        double amount = request["amount"].toDouble();
+        QString errorMessage;
+
+        bool success = JsonDatabase::transferAmount(fromAccountNumber, toAccountNumber, amount, errorMessage);
+        if (success) {
+            response["status"] = "success";
+            response["message"] = "Transfer successful";
+            response["transactions"] = JsonDatabase::getTransactionHistory(request["username"].toString());
+        } else {
+            response["status"] = "failure";
+            response["message"] = errorMessage;
+        }
+
+        sendJsonToSocket(socket, response);
+    }
+}
+
+void TcpServer::disconnected()
+{
+    QTcpSocket* socket = qobject_cast<QTcpSocket*>(sender());
+    if (!socket) {
+        qDebug() << "Invalid socket in disconnected";
+        return;
+    }
+
+    qDebug() << "Client disconnected" << socket;
+    socket->deleteLater();
 }
 
 QJsonObject TcpServer::readJsonFromSocket(QTcpSocket* socket) {

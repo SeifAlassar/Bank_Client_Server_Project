78c15a3343764d420cd8cf523f763bae1af7d3fa
SeifAlassar <132224679+SeifAlassar@users.noreply.github.com>
2024-05-19 00:03:25 +0300

    Update tcpserver.h

--- TCP_Server/tcpserver.h
+++ TCP_Server/tcpserver.h
@@ -4,9 +4,11 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QJsonObject>
-#include <QJsonDocument>
+#include <QMap>
+#include <QThread>
 
-class TcpServer : public QTcpServer {
+class TcpServer : public QTcpServer
+{
     Q_OBJECT
 public:
     explicit TcpServer(QObject *parent = nullptr);
@@ -16,11 +18,15 @@ protected:
     void incomingConnection(qintptr socketDescriptor) override;
 
 private slots:
-    void handleClient(QTcpSocket* socket);
+    void readyRead();
+    void disconnected();
 
 private:
+    void handleClient(QTcpSocket* socket, const QJsonObject& request, QJsonObject& response);
     QJsonObject readJsonFromSocket(QTcpSocket* socket);
     void sendJsonToSocket(QTcpSocket* socket, const QJsonObject &response);
+
+    QMap<QTcpSocket*, QThread*> clientThreads;
 };
 
 #endif // TCPSERVER_H
